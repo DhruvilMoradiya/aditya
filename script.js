4a8f6f25864a87eca4bbdae3a048e3621b7f6818
DhruvilMoradiya <108447840+DhruvilMoradiya@users.noreply.github.com>
2022-07-10 13:47:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Aditya Classes."],
+        strings: ["Aditya Classes"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
